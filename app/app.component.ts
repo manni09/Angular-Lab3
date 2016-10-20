30d6516db27aefead98ba7e2bccef8cb27654dcb
manni09 <manuelkenna@gmail.com>
2016-10-19 19:19:59 -0500

    Some Modification

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,31 @@
+import { Product } from './model/Product';
 import { Component } from '@angular/core';
+
+const Products: Product[] = [
+    { id: 1, name: 'IPod', category: 'Tablet', description: 'Apple Ipod 4th generation' },
+    { id: 2, name: 'IPhone 6', category: 'Phone', description: 'Apple IPhone 6 5th generation' },
+
+    { id: 3, name: 'Samsung Galaxy J5', category: 'Phone', description: 'Samsung Galaxy J5 smartphone with 5.00-inch 720x1280 display powered by 1.2GHz processor alongside 1.5GB RAM and 13-megapixel rear camera.' },
+
+    { id: 4, name: 'Canon DX160', category: 'Camera', description: `The Canon 6354B001 PowerShot SX160 IS 16MP Compact Digital Camera, in black, offers a powerful 16x optical zoom lens enabling you to shoot photos and videos that cover the full spectrum of focal lengths. From wide-angle 28mm shots of friends gathered round the dinner table to standard length shots of landscapes and even telephoto shots of animals, sporting events and close-up portraits, the SX160 has you covered. The Intelligent Optical Image Stabilization system recognizes the different conditions that can cause a shaky, blurry image and corrects them so your long zoom, fast action and low light shots are delivered sharp and stunning. It houses a 16MP CCD sensor and DIGIC 4 Image Processor. The SX160 also captures video in 720p HD and with the HDMI output, you can connect your camera to your HDTV and screen your videos in gorgeous HD moments after you shoot them. If you don't carry your HDTV around with you, use the camera's large 3.0-inch LCD screen to compose and display your images.` },
+
+    { id: 5, name: 'Samsung Galaxy S6', category: 'Phone', description: 'Samsung Galaxy S6 5.1-inch Quad HD Super AMOLED display, 16 MP camera, and a powerful Exynos 7420 processor.' },
+    { id: 6, name: 'Iphone 4S', category: 'Phone', description: 'The Apple iPhone 4S is powered by 800MHz dual-core and it comes with 512MB of RAM. The phone packs 16GB of internal storage cannot be expanded. As far as the cameras are concerned, the Apple iPhone 4S packs a 8-megapixel primary camera on the rear and a 0.3-megapixel front shooter for selfies.' }
+];
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    templateUrl: '/app/template/productDetail.html',
+    styles: ["/styles.css"]
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'Test Product';
+    products = Products;
+    selectedProduct: Product;
+    
+
+    onSelect(product: Product): void {
+        this.selectedProduct = product;
+    }
+}
